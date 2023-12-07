cd816e61aec0576cdac49d0e65beab43560cb005
unknown <aadent@hotmail.com>
2023-12-07 20:28:25 +0000

    initial

---
+++ REACT Project/Instagram Clone/CODEgram/resources/views/app.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title inertia>{{ config('app.name', 'CODEgram') }}</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        <!-- Scripts -->
+        @routes
+        @viteReactRefresh
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @inertiaHead
+    </head>
+    <body class="font-sans antialiased d-flex">
+                <div><img src="/SVG/CODElogo.svg" style="height:20px; border-right: 1px solid #333" class="pr-3" alt="Fancy blue circle"></div>
+        <div class="pl-3">codeGram</div>
+        @inertia
+    </body>
+</html>
